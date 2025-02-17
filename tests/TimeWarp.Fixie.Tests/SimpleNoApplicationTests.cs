7409b1e78aee9aeed919ebdf798bebc4a8585c94
Steven T. Cramer <steven.cramer@timewarp.enterprises>
2025-02-17 22:13:44 +0700

    Replace FluentAssertions with Shouldly

--- tests/TimeWarp.Fixie.Tests/SimpleNoApplicationTests.cs
+++ tests/TimeWarp.Fixie.Tests/SimpleNoApplicationTests.cs
@@ -1,24 +1,24 @@
 ﻿namespace ConventionTest_;
 
-using FluentAssertions;
+using Shouldly;
 using TimeWarp.Fixie;
 
 [TestTag(TestTags.Fast)]
 public class SimpleNoApplicationTest_Should_
 {
-  public static void AlwaysPass() => true.Should().BeTrue();
+  public static void AlwaysPass() => true.ShouldBeTrue();
 
   [Skip("Demonstrates skip attribute")]
-  public static void SkipExample() => true.Should().BeFalse();
+  public static void SkipExample() => true.ShouldBeFalse();
 
   [TestTag(TestTags.Fast)]
-  public static void TagExample() => true.Should().BeTrue();
+  public static void TagExample() => true.ShouldBeTrue();
 
   [Input(5, 3, 2)]
   [Input(8, 5, 3)]
-  public static void Subtract(int aX, int aY, int aExpectedDifference)
+  public static void Subtract(int x, int y, int expectedDifference)
   {
-    int result = aX - aY;
-    result.Should().Be(aExpectedDifference);
+    int result = x - y;
+    result.ShouldBe(expectedDifference);
   }
 }
