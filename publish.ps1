df91be9c2fa43de91e7b7885a00e4ba078ebd073
Steven T. Cramer <steven.cramer@timewarp.enterprises>
2025-02-17 23:26:36 +0700

    use Env for the Nuget Key

--- publish.ps1
+++ publish.ps1
@@ -1,10 +1,10 @@
 Push-Location $PSScriptRoot
 try {
-    if (!$Nuget_Key) { throw "Nuget_Key is not set"}
+    if (!$env:Nuget_Key) { throw "Nuget_Key is not set"}
     dotnet tool restore
     dotnet cleanup -y
     dotnet pack ./source/TimeWarp.Fixie/TimeWarp.Fixie.csproj -c Release
-    dotnet nuget push **/*.nupkg --source https://api.nuget.org/v3/index.json --api-key $Nuget_Key    
+    dotnet nuget push **/*.nupkg --source https://api.nuget.org/v3/index.json --api-key $env:Nuget_Key    
 }
 finally {
     Pop-Location
